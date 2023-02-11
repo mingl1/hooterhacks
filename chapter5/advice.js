12c78dfbd9c742b43df8cb247781443a772166f3
Brian <linbrian24@gmail.com>
2023-02-11 18:30:14 -0500

    Add hole for Chapter 5

--- chapter5/advice.js
+++ chapter5/advice.js
@@ -2,4 +2,5 @@ const button = document.querySelector('.button');
 
 button.addEventListener('click', function() {
     document.getElementById('advice').innerHTML = 'If you hate yourself, remember that you are not alone.<br>A lot of people hate you too.'
+    document.getElementById('jump').src = "/images/hole.jpg";
 })
