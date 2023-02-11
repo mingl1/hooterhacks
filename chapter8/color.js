2ce8b1fc9fa19b5d9d23663c18e261fd7784ff47
Brian <linbrian24@gmail.com>
2023-02-11 18:10:57 -0500

    Add Chapter 8

---
+++ chapter8/color.js
@@ -0,0 +1,23 @@
+let counter = 0;
+
+function color(config) {
+    if (config === 1) {
+        document.getElementById('rose1').src="/images/Rose.png";
+        counter++;
+    }
+
+    if (config === 2) {
+        document.getElementById('rose2').src="/images/Rose.png";
+        counter++;
+    }
+
+    if (config === 3) {
+        document.getElementById('rose3').src="/images/Rose.png";
+        counter++;
+    }
+
+    if (counter === 3) {
+        // Open portal.
+        document.getElementById('help').textContent="Thank you!";
+    }
+}
