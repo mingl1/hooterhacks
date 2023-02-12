068393b6ec70f8d45e35e4989844a7d5733a84c7
Edwin Cheng <edwincheng4840@gmail.com>
2023-02-11 20:06:49 -0500

    Complete chapter 11

---
+++ chapter11/javascript.js
@@ -0,0 +1,13 @@
+const output = document.querySelector('.output');
+
+const yes = document.querySelector('.yes');
+const no = document.querySelector('.no');
+
+yes.addEventListener('click', function() {
+    console.log('hi');
+    window.location.replace('./jail.html');
+})
+
+no.addEventListener('click', function() {
+    output.innerHTML = "You're wrong, please try again!";
+})
