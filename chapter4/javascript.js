796fab9c5fd4702e4185599d1b1a513a7f97d87f
Edwin Cheng <edwincheng4840@gmail.com>
2023-02-11 21:01:25 -0500

    Complete chapter 4

---
+++ chapter4/javascript.js
@@ -0,0 +1,19 @@
+const floatingImage = document.querySelector("#floating-image");
+
+const imageWidth = floatingImage.offsetWidth;
+const imageHeight = floatingImage.offsetHeight;
+
+function moveImage() {
+  const x = Math.floor(Math.random() * (window.innerWidth - imageWidth));
+  const y = Math.floor(Math.random() * (window.innerHeight - imageHeight));
+  floatingImage.style.top = x + "px";
+  floatingImage.style.left = y + "px";
+}
+
+setInterval(moveImage, 1000);
+
+
+const cake = document.querySelector('.cake');
+cake.addEventListener('click', function() {
+    window.location.replace('end/end.html')
+})
